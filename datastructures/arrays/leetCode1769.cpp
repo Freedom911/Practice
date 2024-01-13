23763f2231a993c54aa82c161f8daaf5df0d1195
Srajan Ratti <MsFreedom911@gmail.com>
2024-01-13 10:15:28 +0530

    Update leetCode1769.cpp
    Adding optimized solution

--- datastructures/arrays/leetCode1769.cpp
+++ datastructures/arrays/leetCode1769.cpp
@@ -1,44 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using  std::vector;
-using  std::string;
-
 class Solution {
 public:
-    vector<int> minOperations(string boxes) {
-      
-      vector<int>result;
-      for (int i = 0; i < boxes.length(); i++)
-      {
-        int count = 0;
-        for(int j= i-1,k = i+1;j >=0 || k < boxes.length(); j--,k++)
-        {
-          if(j >= 0 && boxes[j] == '1')
-          {
-            count += std::abs(i-j);
-          }
-          if(k < boxes.length() && boxes[k] == '1')
-          {
-            count += std::abs(i-k);
-          }
-        }
-        result.push_back(count);
-      }
-      
-      return result;
+    vector<int> minOperations(string boxes) 
+	{
+        //using cumulative sum from left side plus right side
+		int n = boxes.size();
+		std::vector<int> ans(n,0);
+		
+		int leftcf = 0, count = 0;
+		for(int i = 0; i < n; i++)
+		{
+			ans[i] = leftcf;
+			
+			if(boxes[i] == '1')
+			{
+				count ++;
+			}
+			leftcf += count;
+		}
+		
+		int rightcf = 0;
+		count = 0;
+		for(int i = n - 1; i >= 0; i--)
+		{
+			ans[i] += rightcf;
+			
+			if(boxes[i] == '1')
+			{
+				count++;
+			}
+			rightcf += count;
+		}
+		
+		
+		return ans;
+		
+    }
+	
+	vector<int> minOperationsN2(string boxes) 
+	{
+	  std::vector<int> ans(boxes.size(),0);
+	  
+	  for(int i = 0; i < boxes.size(); i++)
+	  {
+		for(int j = 0; j < boxes.size(); j++)
+		{
+		  if(i == j)
+		  {
+		    continue;
+		  }
+		  if(boxes[j] == '1')
+		  {
+		    ans[i] += std::abs(j - i);
+		  }
+		}
+	  }
+	  
+	  return ans;
         
     }
 };
-
-int main()
-{
-  Solution s;
-  std::vector<int> r = s.minOperations("001011");
-
-  for(int i = 0; i < r.size(); i++)
-  {
-    std::cout << r[i] << " ";
-  }
-}
