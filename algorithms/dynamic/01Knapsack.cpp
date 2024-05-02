7dfbe64452f1fa0ddcfc62a83230a118f6deb281
Srajan Ratti <MsFreedom911@gmail.com>
2024-05-02 10:35:18 +0530

    Update 01Knapsack.cpp

--- algorithms/dynamic/01Knapsack.cpp
+++ algorithms/dynamic/01Knapsack.cpp
@@ -1,88 +1,112 @@
 //Program For Solving 01 Knapsack using both memoization and top down approach
 //for dp first step is to identify its a dp problem.Generally if there are two are more
 //recursive calls then we can apply dp . Also We need to make the solution optimial is a second requirement
+//0 1knapsack recursive,memoization
 #include <iostream>
+#include <cstring>
 #include <vector>
 
-int t[4][51];
-
-int knapsackRec(std::vector<int> wt,std::vector<int> val,int capacity,int itemsLeft)
+int KnapsackRecursive(const std::vector<int> &wt,const std::vector<int>&val,const int&n,const int&W)
 {
-  //Base Condition
-  if(capacity <= 0 || itemsLeft <= 0)
-    return 0;
-
-  //Check if the result is already present
-  if(t[itemsLeft][capacity] != -1)
-    return t[itemsLeft][capacity] ;
-
-
-  //Now at each step we have choices. If the weight of the last element is greater than capcity then there is only
-  //one choice to not include but if less then we have two option 
-  if(capacity < wt[itemsLeft -1])
-  {
-    return  t[itemsLeft][capacity] = knapsackRec(wt,val,capacity,itemsLeft - 1);
-  }
-
-  else
-  {
-    return  t[itemsLeft][capacity] = std::max(knapsackRec(wt,val,capacity,itemsLeft - 1),//when we dont include the last element
-        val[itemsLeft - 1] + knapsackRec(wt,val,capacity - wt[itemsLeft - 1] ,itemsLeft - 1)); //when we include the last element then reduce the capacity of bag and
-    //also reduce the total items left
-  }
-
+	if(n == 0 || W == 0)
+	{
+		return 0;
+	}
+	
+	//Weight of n - 1 item is less than capacity then we have two choices
+	//either include or exclude
+	if(wt[n-1] <= W)
+	{
+		return std::max((val[n-1] + KnapsackRecursive(wt,val,n-1,W - wt[n-1])),//INclude
+						 KnapsackRecursive(wt,val,n-1,W));
+	}
+	else
+	{
+		return KnapsackRecursive(wt,val,n-1,W);
+	}
 }
 
-int knapsackTopDown(std::vector<int> wt,std::vector<int> val,int capacity)
+int t[5][8];
+int KnapsackMemoization(const std::vector<int> &wt,const std::vector<int>&val,const int&n,const int&W)
 {
-   int mt[4][51];
-
-   //This is the base condition of the recurisve method.
-   //We are initializing with 0
-   for(int i = 0; i < 4; i++)
-   {
-     for(int j = 0; j < 51; j++)
-     {
-       if(i == 0 || j == 0)
-       {
-         mt[i][j] = 0;
-       }
-     }
-   }
-
-   //Similarly we have two conditions here
-   for(int i = 1; i < 4; i++)
-   {
-     for(int j = 1; j < 51; j++)
-     {
-
-       if(wt[i-1] <= j)
-       {
-                             //INclude                    //dont include
-         mt[i][j] = std::max(val[i-1] + mt[i-1][j-wt[i-1]],mt[i-1][j]);
-       }
-       //weight of the bag at i - 1 is greater than j i.e capacity
-       else
-       {
-         mt[i][j] = mt[i-1][j];
-       }
-
-     }
-   }
-
-   return mt[3][50];
+	if(n == 0 || W == 0)
+	{
+		return 0;
+	}
+	
+	if(t[n][W] != -1)
+	{
+		return t[n][W];
+	}
+	
+	//Weight of n - 1 item is less than capacity then we have two choices
+	//either include or exclude
+	if(wt[n-1] <= W)
+	{
+		t[n][W] = std::max((val[n-1] + KnapsackMemoization(wt,val,n - 1,W - wt[n-1])),//INclude
+						 KnapsackMemoization(wt,val,n-1,W));
+		return t[n][W];
+	}
+	else
+	{
+		t[n][W] =  KnapsackMemoization(wt,val,n-1,W);
+		return t[n][W];
+	}
 }
+
+int t1[5][8];
+int KnapsackTabular(const std::vector<int> &wt,const std::vector<int>&val,const int&n,const int&W)
+{
+	
+	for(int i = 0; i < n + 1; i++)
+	{
+		for(int j = 0; j < W + 1; j++)
+		{
+			if(i == 0 || j == 0)
+			{
+				t1[i][j] = 0;
+			}
+		}
+	}
+	
+	for(int i = 1; i < n + 1; i++)
+	{
+		for(int j = 1; j < W + 1; j++)
+		{
+			if(wt[i-1] <= j)
+			{
+				t1[i][j] = std::max(val[i-1] + t[i-1][j-wt[i-1]],t[i-1][j]);
+			}
+			else
+			{
+				t1[i][j] = t[i-1][j];
+			}
+		}
+	}
+	return t1[n][W];
+	
+}
+
 
 
 int main()
 {
-  for(int i = 0; i < 4;i++)
-  {
-    for(int j = 0; j < 51; j++)
-    {
-      t[i][j] = -1;
-    }
-  }
-  std::cout << "\n Maximum Profit = " << knapsackRec({10,20,30},{60,100,120},50,3);
-  std::cout << "\n Maximum Profit = " << knapsackTopDown({10,20,30},{60,100,120},50);
+	std::vector<int> wt{1,3,4,5};
+	std::vector<int> profit{1,4,5,7};
+	int MaxCapacity = 7;
+	
+
+	
+	std::cout << "\n Max Profit Using KnapsackRecursive is = " << KnapsackRecursive(wt,profit,wt.size(),MaxCapacity);
+
+	
+	std::memset(t,-1,sizeof(t));
+	
+	std::cout << "\n Max Profit Using KnapsackMemoization is = " << KnapsackMemoization(wt,profit,wt.size(),MaxCapacity);
+	
+	
+	std::memset(t1,-1,sizeof(t));
+	
+	std::cout << "\n Max Profit Using KnapsackTabular is = " << KnapsackTabular(wt,profit,wt.size(),MaxCapacity);
+	
 }
