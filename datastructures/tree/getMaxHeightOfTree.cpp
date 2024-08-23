d01ec0663e0100c8d414b40fcdcbf1c71a5ff3ca
Srajan Ratti <MsFreedom911@gmail.com>
2024-08-23 11:02:13 +0530

    Update getMaxHeightOfTree added more way

--- datastructures/tree/getMaxHeightOfTree.cpp
+++ datastructures/tree/getMaxHeightOfTree.cpp
@@ -7,6 +7,8 @@
 //https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/
 #include <iostream>
 #include <queue>
+#include <stack>
+#include <limits>
 struct TreeNode
 {
   TreeNode*Left;
@@ -31,7 +33,9 @@ int getHeightIterative(TreeNode *Node)
   while(!q.empty())
   {
     height++;
-
+    
+	//explore all children of the nodes present in queue.
+	//when all children are visited at a particular level then only increment height
     int size = q.size();
     for(int i = 0; i < size; i++)
     {
@@ -47,16 +51,68 @@ int getHeightIterative(TreeNode *Node)
   return height;
 }
 
+//we use post order with double entry to stack
+//we increment height when we insert element in a stack
+//we check max height from there
+//we decrement height when both the children are visited
+int getHeightPostOrderIterative(TreeNode * Root)
+{
+	if(Root == nullptr)
+	{
+		return 0;
+	}
+	int height = 0;
+	int maxHeight = std::numeric_limits<int>::min();
+	
+	std::stack<TreeNode*>st;
+	
+	while(true)
+	{
+		while(Root)
+		{
+			st.push(Root);
+			st.push(Root);
+			Root = Root->Left;
+			height++;
+			maxHeight = std::max(maxHeight,height);
+		}
+		
+		if(st.empty())
+		{
+			return maxHeight;
+		}
+		
+		Root = st.top();
+		st.pop();
+		
+		if(st.size() > 0 && st.top() == Root) // means right child not explored
+		{
+			Root = Root->Right;
+		}
+		else
+		{
+			Root = nullptr;
+			height--;
+		}
+	}
+	return maxHeight;
+}
+
+//using IBH
+//Hypothesis - This  function returns height of tree
 int getHeightRecursive(TreeNode*Node)
 {
+	//Base condition
   if(Node == nullptr)
     return 0;
-
-  int l = getHeightRecursive(Node->Left);
-  int r = getHeightRecursive(Node->Right);
-
+  
+  //Induction step
+  int l = getHeightRecursive(Node->Left); //get height of left subtree
+  int r = getHeightRecursive(Node->Right);//get height of right subtree
+  
+  //out of both we pick max
   if(l > r)
-    return l + 1;
+    return l + 1; //one is for the root node
   else
     return r + 1;
 }
@@ -79,6 +135,7 @@ int main()
   TreeNode Node6 (7);
   TreeNode Node7 (8);
   TreeNode Node8 (9);
+  TreeNode Node9 (10);
 
   Root.Left = &Node1;
   Root.Right = &Node2;
@@ -88,12 +145,14 @@ int main()
   Node4.Right = &Node6;
   Node6.Right = &Node7;
   Node7.Right = &Node8;
+  Node8.Left = &Node9;
 
-  std::cout << "\n Height of tree == " << getHeightRecursive(&Root);
-  std::cout << "\n Height of tree == " << getHeightIterative(&Root) << "\n";;
+  std::cout << "\n Height of tree Recursive == " << getHeightRecursive(&Root);
+  std::cout << "\n Height of tree LEvel Order Iterative == " << getHeightIterative(&Root) << "\n";;
+  
+  std::cout << "\n Height of tree Post Order Iterative == " << getHeightPostOrderIterative(&Root) << "\n";
 
 }
-
 
 
 
