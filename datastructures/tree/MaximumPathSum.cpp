d35f34f9ea178f103c36696b994751d7a9e91171
Srajan Ratti <MsFreedom911@gmail.com>
2024-09-05 11:19:20 +0530

    Update MaximumPathSum.cpp
    Removed DP

--- datastructures/tree/MaximumPathSum.cpp
+++ datastructures/tree/MaximumPathSum.cpp
@@ -61,63 +61,7 @@ int findMaxSum(TreeNode*Root,int &maxAns)
 	
 }
 
-//we use DP here to store result 
-int findMaxSumDP(TreeNode*Root,int &maxAns,std::unordered_map<TreeNode*,int> &us)
-{
-	//Base condition
-	if(Root == nullptr)
-	{
-		return 0;
-	}
-	
-	//Induction Step
-	int l = -1;
-	int r = -1;
-	
-	//using dp to check if result already evaluated
-	if(us.find(Root->Left) != us.end())
-	{
-		l = us[Root->Left];
-	}
-	else
-	{
-		std::cout << "\n Already Evaluated \n";
-		us[Root->Left] = l = findMaxSumDP(Root->Left,maxAns,us);
-	}
-	
-	//using dp to check if result already evaluated
-	if(us.find(Root->Right) != us.end())
-	{
-		std::cout << "\n Already Evaluated \n";
-		r = us[Root->Right];
-	}
-	else
-	{
-		us[Root->Right] = r = findMaxSumDP(Root->Right,maxAns,us);
-	}
-	
 
-	
-	
-	int MainRootInvolved = std::max(Root->Data,std::max(l,r) + Root->Data);
-	
-	//Other thing is we dont want to include the top most root. Root is one only
-	//so we use subtree only and dont invlove root main 
-	//here root ->data is basically the root of subtree
-	int MainRootNotInvolved = l + r + Root->Data;
-	
-	int ans = std::max(MainRootInvolved,MainRootNotInvolved);//pick max of these
-	
-	maxAns = std::max(maxAns,ans);
-	
-	//we pass the result for further analysis.
-	//we dont pass where Main Root is not involved
-	//with hope that we might get better ans than maxAns
-	return MainRootInvolved;
-	
-	
-	
-}
 
 // Driver code
 int main(void)
@@ -136,8 +80,6 @@ int main(void)
 	findMaxSum(root,maxAns);
     std::cout << "Max path sum is " << maxAns << "\n";
 	maxAns = std::numeric_limits<int>::min();
-    std::unordered_map<TreeNode*,int>us;
-	findMaxSumDP(root,maxAns,us);
-	std::cout << "Max path sum DP is " <<  maxAns << "\n";
+
     return 0;
 }
